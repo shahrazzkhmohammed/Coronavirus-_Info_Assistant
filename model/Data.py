e77b3edee561a36a644e6c3a8e571824f46e5518
shahrazzkhmohammed <shahrazzakhmohammed@gmail.com>
2021-04-20 14:54:01 -0400

    Update Data.py

--- model/Data.py
+++ model/Data.py
@@ -1,1 +1,52 @@
+import requests
+import json
 
+class Data:
+	def __init__(self, api_key, project_token):
+		self.api_key = api_key
+		self.project_token = project_token
+		self.params = {
+			"api_key": self.api_key
+		}
+		self.data = self.get_data()
+
+	def get_data(self):
+		response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
+		data = json.loads(response.text)
+		return data
+
+	def get_total_cases(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == "Coronavirus Cases:":
+				return content['value']
+
+	def get_total_deaths(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == "Deaths:":
+				return content['value']
+
+		return "0"
+
+	def get_country_data(self, country):
+		data = self.data["country"]
+
+		for content in data:
+			if content['name'].lower() == country.lower():
+				return content
+
+		return "0"
+
+	def get_list_of_countries(self):
+		countries = []
+		for country in self.data['country']:
+			countries.append(country['name'].lower())
+
+		return countries
+
+	def update_data(self):
+		self.data = self.get_data()
+		return "Data updated\n"
